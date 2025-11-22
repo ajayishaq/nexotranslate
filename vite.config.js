cf1ae80e611e67bcf909ac41a681033693735790
ajayishaq <ishaqmail09@gmail.com>
2025-11-22 04:32:54 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
+      input: { main: 'index.html' }
     }
   },
   server: {
     port: 5000,
     host: '0.0.0.0',
     allowedHosts: true,
-    open: false,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
